3cc939d72ae3af73fd6d3cdd699835bc26ae85a3
Corey Hawthorne <chawthorne02@gmail.com>
2022-09-12 16:01:05 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Codewars practice problems
+
+//1//
+
+var number=function(array){
+  
+    let output = []
+    
+    for(let i = 0; i < array.length; i++) {
+      output.push((i + 1) + ": " + array[i])
+    }
+    return output
+  }
+
+  //2//
+
+  
