f6fd092e23b5b6ca331b07cb4722964d51a5a2b5
Corey Hawthorne <chawthorne02@gmail.com>
2022-09-12 21:27:55 -0400

    all functions complete except 1

--- main.js
+++ main.js
@@ -14,4 +14,102 @@ var number=function(array){
 
   //2//
 
+  const arr = N => Array.from({length: N}, (_,index) => index)
+
   
+  //3//
+
+  function flyBy(lamps, drone){
+    if(drone.length > lamps.length) return 'o'.repeat(lamps.length)
+    
+    return 'o'.repeat(drone.length) + 'x'.repeat(lamps.length-drone.length)
+  }
+
+  //4//
+
+  function getAverage(marks){
+    return Math.floor(marks.reduce((c,a) => c+a ) / marks.length) 
+    }
+
+//5//
+
+function array(arr) {
+    return arr.reverse()
+}
+
+
+
+// JS-Array-Exercises
+
+//1//
+
+function array(arr) {
+    let i = 0
+
+    for(let i = 0; i < arr.length; i++) {
+        
+    }
+}
+
+
+
+//2//
+
+function array(arr) {
+    const reversed1 = arr.slice().reverse();
+
+    return reversed1
+}
+
+//3//
+
+function array(arr) {
+    return arr.filter(Boolean)
+}
+
+//4//
+
+
+
+
+//5//
+
+function array(arr) {
+    let newArray = [new Set(arr)];
+
+    return newArray
+}
+
+
+//6//
+
+function equal(arr1, arr2) {
+    let i = 0
+
+    for (let i = 0; i < arr1 && arr2; i++) {
+        if(arr1 === arr2) {
+            return true
+        }
+    else {
+        return false
+    }
+    }
+}
+
+//BONUS//
+
+function array(arr1, arr2) {
+    return arr1.concat(arr2)
+}
+
+///////
+
+function array(arr, size) {
+    const res = [];
+
+    for(let i = 0; i < arr.length; i += size) {
+        const portion = arr.slice(i, i + size);
+        res.push(size);
+    }
+    return res
+}
